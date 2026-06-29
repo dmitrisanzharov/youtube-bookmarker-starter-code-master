1866b6e6a136a2427e2a434bd526b1b8936e1c79
VictorSarov <victor.sarov@rentalmatics.com>
2026-06-29 09:00:53 +0100

    all good

--- background.js
+++ background.js
@@ -1,18 +1,16 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+console.log("hello starting console");
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.url?.includes("youtube.com")) {
-        console.log("YouTube tab opened/loaded:", tab.url);
-    }
-});
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//     if (tab.url && tab.url.includes("*youtube.com*")) {
+//       console.log("hello");
+//     }
+//     if (tab.url && tab.url.includes("youtube.com/watch")) {
+//       const queryParameters = tab.url.split("?")[1];
+//       const urlParameters = new URLSearchParams(queryParameters);
+  
+//       chrome.tabs.sendMessage(tabId, {
+//         type: "NEW",
+//         videoId: urlParameters.get("v"),
+//       });
+//     }
+//   });
