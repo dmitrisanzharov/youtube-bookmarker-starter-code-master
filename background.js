aa9f5f214a3bf92e78c866be72605b6c123a8395
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-03 08:26:04 +0100

    all good

--- background.js
+++ background.js
@@ -4,9 +4,11 @@ console.log('background.js loaded');
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   
-    if (changeInfo.status === 'complete' && tab.url.includes('youtube.com/watch')) {
+    if (changeInfo.status === 'complete' && tab.url.includes('youtube.com')) {
         console.log('YouTube video page loaded:', tab.url);
     }
+
+    chrome.tabs.sendMessage(tabId, { tabId: tabId, message: 'hello from background.js' });
 
 
 });
