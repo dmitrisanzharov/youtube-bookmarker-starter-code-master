b7dc4397d42250c7b72143e85d0f25a4e4b8d9ce
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-05 11:48:08 +0100

    good stuff

--- foo.js
+++ foo.js
@@ -1,10 +1,4 @@
 const mainUrl = 'https://www.youtube.com/watch?v=hvPGfcAgk9Y&list=PLPNW_gerXa4OoypUEgZI7uouI12WZrxeS&index=2';
 
-const searchUrl = new URL(mainUrl);
-console.log('searchUrl: ', searchUrl);
-
-const searchUrl1 = new URL(mainUrl).searchParams;
-console.log('searchUrl1: ', searchUrl1);
-
-const obj = Object.fromEntries(searchUrl1.entries());
-console.log("obj: ", obj);
+const newUrl = Object.fromEntries(new URL(mainUrl).searchParams.entries());
+console.log("newUrl: ", newUrl);
