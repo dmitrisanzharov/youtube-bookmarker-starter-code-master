c44e5e27dd1b94f8544e0c3b6d2f7d2f4f2b3d95
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-06 07:57:52 +0100

    good stuff

--- my_content.js
+++ my_content.js
@@ -1,6 +1,35 @@
 (async () => {
     console.log('my_content.js loaded');
 
+    function makeDevSpace() {
+        const devSpaceDiv = document.createElement('div');
+        Object.assign(devSpaceDiv.style, {
+            position: 'fixed',
+            bottom: '0',
+            left: '0',
+            width: '100%',
+            background: 'gray',
+            color: 'black',
+            fontSize: '20px',
+            zIndex: '999999',
+            padding: '8px'
+        });
+        // devSpaceDiv.textContent = 'Dev Space: This is a development space for testing and debugging.';
+        document.body.appendChild(devSpaceDiv);
+
+        // add button
+        const bookmarkBtn = document.createElement('img');
+        bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png');
+        bookmarkBtn.className = 'ytp-button ' + 'bookmark-btn';
+        bookmarkBtn.title = 'Click to bookmark current timestamp';
+        Object.assign(bookmarkBtn.style, {
+            width: '60px',
+            height: '60px'
+        });
+        devSpaceDiv.appendChild(bookmarkBtn);
+    }
+
+    makeDevSpace();
 
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         console.log('my_content.js received message:', message);
